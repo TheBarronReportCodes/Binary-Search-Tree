cd1f43c43da9229016395c0443a7d41dbf259718
jbarron8 <jbarron8@cs6261-vm18.uwg.westga.edu>
2020-06-07 15:49:59 -0400

    testing to  see if brick.size and brick.color are of correct type

--- test/testBrickConstructor.js
+++ test/testBrickConstructor.js
@@ -3,14 +3,24 @@ var lego = require('../legos');
 const Brick = lego.Brick;
 
 describe('testBrickConstructor', function() {
-    it('assert that size of brick has value of int', function() {
+    it('assert that size of brick is of type int', function() {
 	var brick1 = new Brick(1,"test");
-	var result = return brick1.size.valueof();
-      assert.equal(result, int);
+	var result = brick1.size;
+      assert.equal(result, 'int');
     });
-    it('assert that color of brick has value of string', function() {
+    it('assert that color of brick is of type string', function() {
 	var brick1 = new Brick(1,"test");
-	var result = return brick1.size.valueof();
-      assert.equal(console.log(brick.size), 62);
+	var result = brick1.color
+      assert.typeOf(result, 'string');
+    });
+    it('assert that value of size of brick is 1', function() {
+	var brick1 = new Brick(1, "test");
+	var result = brick1.size;
+      assert.equal(result, 1);
+    });
+    it('assert that value of color of brick is test', function() {
+	var brick1 = new Brick(1, "test");
+	var result = brick1.color
+      assert.equal(result, "test");
     });
 });
