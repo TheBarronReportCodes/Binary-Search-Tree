29425f106b19601e6cdf69a99259264ac55ecd3b
jbarron8 <jbarron8@cs6261-vm23.uwg.westga.edu>
2020-06-07 22:07:45 -0400

    root with left child and root with right child tested and passed. Beginning root with both left and right children

--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -13,17 +13,35 @@ describe('testBST', function() {
       assert.equal(tree0.root.data, brick0);
     });
 
-    it('assert that brick0 left node inserted into the root node is null', function() {
+    it('assert that brick0(root node) left is null', function() {
 	var brick0 = new Brick(1,"test");
 	var tree0 = new BST();
 	tree0.insert(brick0);
       assert.equal(tree0.root.left, null);
     });
 
-    it('assert that brick0 right node inserted into the root node is null', function() {
+    it('assert that brick0(root node) right is null', function() {
 	var brick0 = new Brick(1,"test");
 	var tree0 = new BST();
 	tree0.insert(brick0);
       assert.equal(tree0.root.right, null);
+    });
+
+    it('assert that two bricks(sizes 102 and 65) inserted into BST end with brick object of size 65 as the root nodes left child', function() {
+	var brick0 = new Brick(102,"red");
+	var brick1 = new Brick(65,"red");
+	var tree0 = new BST();
+	tree0.insert(brick0);
+	tree0.insert(brick1);
+      assert.equal(tree0.root.left.data, brick1);
+    });
+
+    it('assert that two bricks(sizes 65 and) inserted into BST end with brick object of size 102 as the root nodes right child', function() {
+	var brick0 = new Brick(65,"red");
+	var brick1 = new Brick(102,"red");
+	var tree0 = new BST();
+	tree0.insert(brick0);
+	tree0.insert(brick1);
+      assert.equal(tree0.root.right.data, brick1);
     });
 });
