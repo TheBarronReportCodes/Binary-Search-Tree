2cc87a47a3c247813ebed4fd5a7eeb2adbaab16d
jbarron8 <jbarron8@cs6261-vm05.uwg.westga.edu>
2020-06-08 23:48:21 -0400

    add, find, remove, and show all functions created. Need to create insert function

--- legos.js
+++ legos.js
@@ -10,10 +10,42 @@ this.color = color;
 
 /* Creates a LegoPile where the pile contains bricks of different colors
 */
-function LegoPile(Brick) {
+function LegoPile() {
 this.add = add;
 this.datastore = new Array();
 this.find = find;
 this.remove = remove;
 this.showAll = showAll;
+this.insert = insert;
+}
+
+/* adds a specified value to the key. The key serves as a location inside of the datastore array. 
+*/
+function add(key, value) {
+this.datastore[key] = value;
+}
+
+/* finds and returns the value at the specified key
+*/
+function find(key) {
+return this.datastore[key];
+}
+
+/* delete the value at the specified key
+*/
+function remove(key) {
+delete this.datastore[key];
+}
+
+/* show all the keys and their value
+*/
+function showAll() {
+	for (var key in this.datastore) {
+	print(key + "->" + this.datastore[key]);
+	}
+}
+
+/* inserts a brick object into the appropriate colors BST
+*/
+function insert(Brick) {
 }
