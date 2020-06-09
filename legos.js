ca44a7b6480858eb93e658074df405c9b680ef34
jbarron8 <jbarron8@cs6261-vm22.uwg.westga.edu>
2020-06-09 10:28:12 -0400

    implemented insert(Brick funtion). Adding in specific color keys then beginning testing

--- legos.js
+++ legos.js
@@ -1,6 +1,9 @@
 module.exports.Brick = Brick;
 module.exports.LegoPile = LegoPile;
 
+var bst = require('../bst');
+const BST = bst.BST;
+
 /* Creates a brick object of a specific size and color
 */
 function Brick(size, color) {
@@ -48,4 +51,9 @@ function showAll() {
 /* inserts a brick object into the appropriate colors BST
 */
 function insert(Brick) {
+	for (var key in this.datastore) {
+		if (Brick.color == key) {
+		this.datastore[key].insert(Brick);
+		}
+	}
 }
