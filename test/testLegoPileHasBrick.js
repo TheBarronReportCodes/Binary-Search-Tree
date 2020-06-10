912fce2908b248a24a08a76f2b50b8059bf37aac
jbarron8 <jbarron8@cs6261-vm26.uwg.westga.edu>
2020-06-10 11:13:14 -0400

    beginning test on legoPileHasBrick

---
+++ test/testLegoPileHasBrick.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var lego = require('../legos');
+
+const Brick = lego.Brick;
+const LegoPile = lego.LegoPile
+
+describe('testLegoPileHasBrick', function() {
+	describe('insert one brick', function() {
+		it('assert that ', function() {
+
+		});
+	});
+});
