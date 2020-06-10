0119ff2a75e0816556e23a21eee2dc83cc14f15e
jbarron8 <jbarron8@cs6261-vm26.uwg.westga.edu>
2020-06-10 13:32:21 -0400

    testing for second case

--- test/testLegoPileHasBrick.js
+++ test/testLegoPileHasBrick.js
@@ -13,4 +13,20 @@ describe('testLegoPileHasBrick', function() {
 			assert.equal(pile.hasBrick(60, "green"), true);
 		});
 	});
+
+	describe('find two bricks in different trees', function() {
+		it('assert that the second brick inserted into each of the specified BST is found', function() {
+			var brick0 = new Brick(60, "green");
+			var brick1 = new Brick(65, "green");
+			var brick2 = new Brick(80, "black");
+			var brick3 = new Brick(75, "black");
+			var pile = new LegoPile();
+			pile.insert(brick0);
+			pile.insert(brick1);
+			pile.insert(brick2);
+			pile.insert(brick3);
+			assert.equal(pile.hasBrick(65, "green"), true);
+			assert.equal(pile.hasBrick(75, "green"), true);
+		});
+	});
 });
