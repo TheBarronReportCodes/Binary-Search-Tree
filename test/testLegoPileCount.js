d3b7d670b722eb9e63584ed945a9b78ac19b469d
jbarron8 <jbarron8@cs6261-vm16.uwg.westga.edu>
2020-06-11 15:09:56 -0400

    test case 2 (4 bricks inserted) passed. Moving on to test 3. Then double checking work, then finished

--- test/testLegoPileCount.js
+++ test/testLegoPileCount.js
@@ -13,4 +13,19 @@ describe('testLegoPileCount', function() {
 			assert.equal(pile.legoPileCount(), 1);
 		});
 	});
+
+	describe('find four bricks', function() {
+		it('assert that the four inserted bricks are found in the pile', function() {
+			var brick0 = new Brick(60, "blue");
+			var brick1 = new Brick(75, "green");
+			var brick2 = new Brick(80, "green");
+			var brick3 = new Brick(33, "red");
+			var pile = new LegoPile();
+			pile.insert(brick0);
+			pile.insert(brick1);
+			pile.insert(brick2);
+			pile.insert(brick3);
+			assert.equal(pile.legoPileCount(), 4);
+		});
+	});
 });
