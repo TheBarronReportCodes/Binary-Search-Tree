f2a85eaea8cf0177e6fd00630e9d3ac987910c6c
jbarron8 <jbarron8@cs6261-vm24.uwg.westga.edu>
2020-06-11 12:33:08 -0400

    test case 3 (multiple bricks inserted into a BST) passed. count and count subtree functions passed multiple test cases. Moving onto count and subtree function for LegoPile

--- test/testBstCount.js
+++ test/testBstCount.js
@@ -16,13 +16,32 @@ describe('testBSTcount', function() {
 	});
 
 	describe('count two bricks', function() {
-    		it('assert that the count of nodes for a bst with two brick returns 2', function() {
+    		it('assert that the count of nodes for a bst with two bricks returns 2', function() {
 		var brick0 = new Brick(50, "red");
 		var brick1 = new Brick(65, "red");
 		var tree = new BST();
 		tree.insert(brick0);
 		tree.insert(brick1);
 		assert.equal(tree.count(), 2);
+		});
+    	});
+
+	describe('count multiple bricks', function() {
+    		it('assert that the count of nodes for a bst with six bricks returns 6', function() {
+		var brick0 = new Brick(50, "red");
+		var brick1 = new Brick(65, "red");
+		var brick2 = new Brick(33, "red");
+		var brick3 = new Brick(96, "red");
+		var brick4 = new Brick(11, "red");
+		var brick5 = new Brick(45, "red");
+		var tree = new BST();
+		tree.insert(brick0);
+		tree.insert(brick1);
+		tree.insert(brick2);
+		tree.insert(brick3);
+		tree.insert(brick4);
+		tree.insert(brick5);
+		assert.equal(tree.count(), 6);
 		});
     	});
 });
