b07f0be5e51f87bdfa1b1845c0518ad68ee18026
jbarron8 <jbarron8@cs6261-vm16.uwg.westga.edu>
2020-06-11 15:17:43 -0400

    final test case completed and passed. Double checking then submitting

--- test/testLegoPileCount.js
+++ test/testLegoPileCount.js
@@ -28,4 +28,31 @@ describe('testLegoPileCount', function() {
 			assert.equal(pile.legoPileCount(), 4);
 		});
 	});
+
+	describe('find multiple bricks within multple BSTs', function() {
+		it('assert that the 10 inserted bricks are found in the pile', function() {
+			var brick0 = new Brick(60, "white");
+			var brick1 = new Brick(75, "green");
+			var brick2 = new Brick(80, "green");
+			var brick3 = new Brick(33, "green");
+			var brick4 = new Brick(6, "green");
+			var brick5 = new Brick(75, "green");
+			var brick6 = new Brick(80, "yellow");
+			var brick7 = new Brick(33, "red");
+			var brick8 = new Brick(60, "blue");
+			var brick9 = new Brick(75, "black");
+			var pile = new LegoPile();
+			pile.insert(brick0);
+			pile.insert(brick1);
+			pile.insert(brick2);
+			pile.insert(brick3);
+			pile.insert(brick4);
+			pile.insert(brick5);
+			pile.insert(brick6);
+			pile.insert(brick7);
+			pile.insert(brick8);
+			pile.insert(brick9);
+			assert.equal(pile.legoPileCount(), 10);
+		});
+	});
 });
