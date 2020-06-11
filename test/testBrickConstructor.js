3ce9d341f333ee418c5b5e09443ed325ca3ff9be
jbarron8 <jbarron8@cs6261-vm16.uwg.westga.edu>
2020-06-11 15:52:47 -0400

    fixed format and indentation. Finished

--- test/testBrickConstructor.js
+++ test/testBrickConstructor.js
@@ -3,38 +3,38 @@ var lego = require('../legos');
 const Brick = lego.Brick;
 
 describe('testBrickConstructor', function() {
-describe('test typeof', function() {
-    it('assert that size of brick is of type number', function() {
-	var brick1 = new Brick(1,"test");
-	var result = brick1.size;
-      assert.equal(typeof result, 'number');
-    });
-    it('assert that color of brick is of type string', function() {
-	var brick1 = new Brick(1,"test");
-	var result = brick1.color
-      assert.equal(typeof result, 'string');
-    });
-});
-describe('test value', function() {
-    it('assert that value of size of brick is 1', function() {
-	var brick1 = new Brick(1, "test");
-	var result = brick1.size;
-      assert.equal(result, 1);
-    });
-    it('assert that value of color of brick is test', function() {
-	var brick1 = new Brick(1, "test");
-	var result = brick1.color
-      assert.equal(result, "test");
-    });
-    it('assert that value of size of brick is 10', function() {
-	var brick1 = new Brick(10, "red");
-	var result = brick1.size;
-      assert.equal(result, 10);
-    });
-    it('assert that value of color of brick is yellow', function() {
-	var brick1 = new Brick(67, "yellow");
-	var result = brick1.color
-      assert.equal(result, "yellow");
-    });
-});
+	describe('test typeof', function() {
+    		it('assert that size of brick is of type number', function() {
+		var brick1 = new Brick(1,"test");
+		var result = brick1.size;
+      		assert.equal(typeof result, 'number');
+    		});
+    		it('assert that color of brick is of type string', function() {
+		var brick1 = new Brick(1,"test");
+		var result = brick1.color
+      		assert.equal(typeof result, 'string');
+    		});
+	});
+	describe('test value', function() {
+    		it('assert that value of size of brick is 1', function() {
+		var brick1 = new Brick(1, "test");
+		var result = brick1.size;
+      		assert.equal(result, 1);
+    		});
+    		it('assert that value of color of brick is test', function() {
+		var brick1 = new Brick(1, "test");
+		var result = brick1.color
+      		assert.equal(result, "test");
+    		});
+    		it('assert that value of size of brick is 10', function() {
+		var brick1 = new Brick(10, "red");
+		var result = brick1.size;
+      		assert.equal(result, 10);
+    		});
+    		it('assert that value of color of brick is yellow', function() {
+		var brick1 = new Brick(67, "yellow");
+		var result = brick1.color
+      		assert.equal(result, "yellow");
+    		});
+	});
 });
--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -6,87 +6,88 @@ const Brick = lego.Brick;
 const BST = bst.BST;
 
 describe('testBST', function() {
-describe('test root', function() {
-    it('assert that brick0 is inserted into the root node', function() {
-	var brick0 = new Brick(1,"test");
-	var tree0 = new BST();
-	tree0.insert(brick0);
-      assert.equal(tree0.root.data, brick0);
-    });
+	describe('test root', function() {
+    		it('assert that brick0 is inserted into the root node', function() {
+		var brick0 = new Brick(1,"test");
+		var tree0 = new BST();
+		tree0.insert(brick0);
+      		assert.equal(tree0.root.data, brick0);
+    		});
 
-    it('assert that brick0(root node) left is null', function() {
-	var brick0 = new Brick(1,"test");
-	var tree0 = new BST();
-	tree0.insert(brick0);
-      assert.equal(tree0.root.left, null);
-    });
+    		it('assert that brick0(root node) left is null', function() {
+		var brick0 = new Brick(1,"test");
+		var tree0 = new BST();
+		tree0.insert(brick0);
+      		assert.equal(tree0.root.left, null);
+    		});
 
-    it('assert that brick0(root node) right is null', function() {
-	var brick0 = new Brick(1,"test");
-	var tree0 = new BST();
-	tree0.insert(brick0);
-      assert.equal(tree0.root.right, null);
-    });
-});
-describe('test other nodes in tree', function() {
-    it('assert that two bricks(sizes 102 and 65) inserted into BST end with brick object of size 65 as the root nodes left child', function() {
-	var brick0 = new Brick(102,"red");
-	var brick1 = new Brick(65,"red");
-	var tree0 = new BST();
-	tree0.insert(brick0);
-	tree0.insert(brick1);
-      assert.equal(tree0.root.left.data, brick1);
-    });
+    		it('assert that brick0(root node) right is null', function() {
+		var brick0 = new Brick(1,"test");
+		var tree0 = new BST();
+		tree0.insert(brick0);
+      		assert.equal(tree0.root.right, null);
+    		});
+	});
 
-    it('assert that two bricks(sizes 65 and 102) inserted into BST end with brick object of size 102 as the root nodes right child', function() {
-	var brick0 = new Brick(65,"red");
-	var brick1 = new Brick(102,"red");
-	var tree0 = new BST();
-	tree0.insert(brick0);
-	tree0.insert(brick1);
-      assert.equal(tree0.root.right.data, brick1);
-    });
+	describe('test other nodes in tree', function() {
+    		it('assert that two bricks(sizes 102 and 65) inserted into BST end with brick object of size 65 as the root nodes left child', function() {
+		var brick0 = new Brick(102,"red");
+		var brick1 = new Brick(65,"red");
+		var tree0 = new BST();
+		tree0.insert(brick0);
+		tree0.insert(brick1);
+      		assert.equal(tree0.root.left.data, brick1);
+    		});
 
-    it('assert that three bricks(sizes 65, 102, and 33) inserted into BST end with brick object of size 102 as the root nodes right child and brick object of size 33 as the root nodes left child', function() {
-	var brick0 = new Brick(65,"red");
-	var brick1 = new Brick(102,"red");
-	var brick2 = new Brick(33,"red");
-	var tree0 = new BST();
-	tree0.insert(brick0);
-	tree0.insert(brick1);
-	tree0.insert(brick2);
-      assert.equal(tree0.root.right.data, brick1);
-      assert.equal(tree0.root.left.data, brick2);
-    });
+    		it('assert that two bricks(sizes 65 and 102) inserted into BST end with brick object of size 102 as the root nodes right child', function() {
+		var brick0 = new Brick(65,"red");
+		var brick1 = new Brick(102,"red");
+		var tree0 = new BST();
+		tree0.insert(brick0);
+		tree0.insert(brick1);
+      		assert.equal(tree0.root.right.data, brick1);
+    		});
 
-    it('assert that nine bricks(sizes 50, 86, 41, 69, 77, 2, 27, 24, 10) inserted into BST are arranged in appropriate order, starting with brick size 50 as the root and brick size 2 at the left most side of the tree', function() {
-	var brick0 = new Brick(50,"red");
-	var brick1 = new Brick(86,"red");
-	var brick2 = new Brick(41,"red");
-	var brick3 = new Brick(69,"red");
-	var brick4 = new Brick(77,"red");
-	var brick5 = new Brick(2,"red");
-	var brick6 = new Brick(27,"red");
-	var brick7 = new Brick(24,"red");
-	var brick8 = new Brick(10,"red");
-	var tree0 = new BST();
-	tree0.insert(brick0);
-	tree0.insert(brick1);
-	tree0.insert(brick2);
-	tree0.insert(brick3);
-	tree0.insert(brick4);
-	tree0.insert(brick5);
-	tree0.insert(brick6);
-	tree0.insert(brick7);
-	tree0.insert(brick8);
-      assert.equal(tree0.root.right.data, brick1);
-      assert.equal(tree0.root.left.data, brick2);
-      assert.equal(tree0.root.right.left.data, brick3);
-      assert.equal(tree0.root.right.left.right.data, brick4);
-      assert.equal(tree0.root.left.left.data, brick5);
-      assert.equal(tree0.root.left.left.right.data, brick6);
-      assert.equal(tree0.root.left.left.right.left.data, brick7);
-      assert.equal(tree0.root.left.left.right.left.left.data, brick8);
-    });
-});
+    		it('assert that three bricks(sizes 65, 102, and 33) inserted into BST end with brick object of size 102 as the root nodes right child and brick object of size 33 as the root nodes left child', function() {
+		var brick0 = new Brick(65,"red");
+		var brick1 = new Brick(102,"red");
+		var brick2 = new Brick(33,"red");
+		var tree0 = new BST();
+		tree0.insert(brick0);
+		tree0.insert(brick1);
+		tree0.insert(brick2);
+      		assert.equal(tree0.root.right.data, brick1);
+      		assert.equal(tree0.root.left.data, brick2);
+    		});
+
+    		it('assert that nine bricks(sizes 50, 86, 41, 69, 77, 2, 27, 24, 10) inserted into BST are arranged in appropriate order, starting with brick size 50 as the root and brick size 2 at the left most side of the tree', function() {
+		var brick0 = new Brick(50,"red");
+		var brick1 = new Brick(86,"red");
+		var brick2 = new Brick(41,"red");
+		var brick3 = new Brick(69,"red");
+		var brick4 = new Brick(77,"red");
+		var brick5 = new Brick(2,"red");
+		var brick6 = new Brick(27,"red");
+		var brick7 = new Brick(24,"red");
+		var brick8 = new Brick(10,"red");
+		var tree0 = new BST();
+		tree0.insert(brick0);
+		tree0.insert(brick1);
+		tree0.insert(brick2);
+		tree0.insert(brick3);
+		tree0.insert(brick4);
+		tree0.insert(brick5);
+		tree0.insert(brick6);
+		tree0.insert(brick7);
+		tree0.insert(brick8);
+      		assert.equal(tree0.root.right.data, brick1);
+      		assert.equal(tree0.root.left.data, brick2);
+      		assert.equal(tree0.root.right.left.data, brick3);
+      		assert.equal(tree0.root.right.left.right.data, brick4);
+      		assert.equal(tree0.root.left.left.data, brick5);
+      		assert.equal(tree0.root.left.left.right.data, brick6);
+      		assert.equal(tree0.root.left.left.right.left.data, brick7);
+      		assert.equal(tree0.root.left.left.right.left.left.data, brick8);
+    		});
+	});
 });
